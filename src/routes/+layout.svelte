af99c8c1b1fdf69f46f4f76c6cfe451edbe95d05
leucas <sodapanda20@gmail.com>
2023-02-07 23:28:11 +0800

    [add]init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '../app.css';
+	import { SvelteUIProvider } from '@svelteuidev/core';
+</script>
+
+<SvelteUIProvider>
+	<slot />
+</SvelteUIProvider>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import { onMount } from 'svelte';
+	import { Button } from '@svelteuidev/core';
+	import { of, map } from 'rxjs';
+
+	let msg = '';
+	onMount(() => {
+		console.log('mount');
+	});
+
+	function handleClick() {
+		of('1', '2', '3')
+			.pipe(map((text) => `${msg} ${text}`))
+			.subscribe((v) => {
+				msg = v;
+			});
+	}
+</script>
+
+<Button on:click={handleClick}>Settings</Button>
+{msg}
